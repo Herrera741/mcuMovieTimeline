5d70bff62b6fe19d88e195c43b1eb20af9dc852f
Sergio Herrera <sherrera725@csu.fullerton.edu>
2021-02-21 12:31:46 -0800

    Initial Commit

---
+++ mcuMovieTimeline/mcuMovieTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  mcuMovieTimelineApp.swift
+//  mcuMovieTimeline
+//
+//  Created by CSUFTitan on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct mcuMovieTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
